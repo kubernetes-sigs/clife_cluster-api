njuettner/cluster-api

bootstrap/kubeadm/api/v1alpha3: fix fuzzing tests for bootstrap tokens
Co-authored-by: Joel Speed <joel.speed@hotmail.co.uk>

Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

bootstrap/kubeadm/api/v1alpha3/conversion_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha3
 
 import (
+	"math/rand"
 	"testing"
 
 	fuzz "github.com/google/gofuzz"
@@ -25,6 +26,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1alpha4"
+	kubeadmv1beta1 "sigs.k8s.io/cluster-api/bootstrap/kubeadm/types/v1beta1"
 	utilconversion "sigs.k8s.io/cluster-api/util/conversion"
 )
 
@@ -34,8 +36,8 @@ func TestFuzzyConversion(t *testing.T) {
 	g.Expect(AddToScheme(scheme)).To(Succeed())
 	g.Expect(v1alpha4.AddToScheme(scheme)).To(Succeed())
 
-	t.Run("for KubeadmConfig", utilconversion.FuzzTestFunc(scheme, &v1alpha4.KubeadmConfig{}, &KubeadmConfig{}, KubeadmConfigStatusFuzzFuncs))
-	t.Run("for KubeadmConfigTemplate", utilconversion.FuzzTestFunc(scheme, &v1alpha4.KubeadmConfigTemplate{}, &KubeadmConfigTemplate{}))
+	t.Run("for KubeadmConfig", utilconversion.FuzzTestFunc(scheme, &v1alpha4.KubeadmConfig{}, &KubeadmConfig{}, KubeadmConfigStatusFuzzFuncs, kubeadmFuzzerFuncs))
+	t.Run("for KubeadmConfigTemplate", utilconversion.FuzzTestFunc(scheme, &v1alpha4.KubeadmConfigTemplate{}, &KubeadmConfigTemplate{}, kubeadmFuzzerFuncs))
 }
 
 func KubeadmConfigStatusFuzzFuncs(_ runtimeserializer.CodecFactory) []interface{} {
@@ -49,4 +51,33 @@ func KubeadmConfigStatusFuzzer(obj *KubeadmConfigStatus, c fuzz.Continue) {
 
 	// KubeadmConfigStatus.BootstrapData has been removed in v1alpha4, so setting it to nil in order to avoid v1alpha3 --> v1alpha4 --> v1alpha3 round trip errors.
 	obj.BootstrapData = nil
+}
+
+func kubeadmFuzzerFuncs(codecs runtimeserializer.CodecFactory) []interface{} {
+	return []interface{}{
+		// Fuzzer for BootstrapToken to ensure correctness of the token format.
+		func(j **kubeadmv1beta1.BootstrapTokenString, c fuzz.Continue) {
+			if c.RandBool() {
+				t := &kubeadmv1beta1.BootstrapTokenString{}
+				c.Fuzz(t)
+
+				t.ID = randTokenString(6)
+				t.Secret = randTokenString(16)
+
+				*j = t
+			} else {
+				*j = nil
+			}
+		},
+	}
+}
+
+const tokenCharsBytes = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+func randTokenString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = tokenCharsBytes[rand.Intn(len(tokenCharsBytes))]
+	}
+	return string(b)
 }

kubernetes-sigs/clife_cluster-api

bootstrap/kubeadm/api/v1alpha3/conversion_test.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1alpha3
 
 import (
+	"math/rand"
 	"testing"
 
 	fuzz "github.com/google/gofuzz"
@@ -41,11 +42,11 @@ func TestFuzzyConversion(t *testing.T) {
 }
 
 func fuzzFuncs(_ runtimeserializer.CodecFactory) []interface{} {
-	return []interface{}{
+	return append([]interface{}{
 		KubeadmConfigStatusFuzzer,
 		dnsFuzzer,
 		clusterConfigurationFuzzer,
-	}
+	}, bootstrapTokenFuzzers()...)
 }
 
 func KubeadmConfigStatusFuzzer(obj *KubeadmConfigStatus, c fuzz.Continue) {
@@ -67,4 +68,46 @@ func clusterConfigurationFuzzer(obj *v1beta1.ClusterConfiguration, c fuzz.Contin
 
 	// ClusterConfiguration.UseHyperKubeImage has been removed in v1alpha4, so setting it to false in order to avoid v1beta1 --> v1alpha4 --> v1beta1 round trip errors.
 	obj.UseHyperKubeImage = false
+}
+
+func bootstrapTokenFuzzers() []interface{} {
+	return []interface{}{
+		// Fuzzer for BootstrapToken to ensure correctness of the token format.
+		func(j **v1beta1.BootstrapTokenString, c fuzz.Continue) {
+			if c.RandBool() {
+				t := &v1beta1.BootstrapTokenString{}
+				c.Fuzz(t)
+
+				t.ID = randTokenString(6)
+				t.Secret = randTokenString(16)
+
+				*j = t
+			} else {
+				*j = nil
+			}
+		},
+		func(j **v1alpha4.BootstrapTokenString, c fuzz.Continue) {
+			if c.RandBool() {
+				t := &v1alpha4.BootstrapTokenString{}
+				c.Fuzz(t)
+
+				t.ID = randTokenString(6)
+				t.Secret = randTokenString(16)
+
+				*j = t
+			} else {
+				*j = nil
+			}
+		},
+	}
+}
+
+const tokenCharsBytes = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+func randTokenString(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = tokenCharsBytes[rand.Intn(len(tokenCharsBytes))]
+	}
+	return string(b)
 }